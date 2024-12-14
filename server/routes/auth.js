cc171237bb201fd79fe9cbe8d980805b5bf69757
AdrifeCova <adrife2000@hotmail.com>
2024-12-14 14:58:00 -0600

    refactor: middleware

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { getUser, loginUser, registerUser, updateUser } from '../controllers/authController.js';
+import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/', getUser)
+router.get('/', protect, getUser)
 router.post('/register', registerUser)
 router.post('/login', loginUser);
-router.put('/:id', updateUser)
+router.put('/:id', protect, updateUser)
 
 export default router;
