e43418ce161d746b67106c95dae76db3058588b3
AdrifeCova <adrife2000@hotmail.com>
2024-12-13 19:24:39 -0600

    refactor: finished authController function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,25 +1,22 @@
 import asyncHandler from 'express-async-handler';
 import { AppError } from '../utils/AppError.js';
 import User from '../models/User.js';
-import jwt from 'jsonwebtoken';
+import generateJWT from '../utils/generateToken.js'
+import bcrypt from 'bcrypt'
 
 export const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  const user = await User.findOne({ email });
-
-  if (!user || !(await user.comparePassword(password))) {
-    throw new AppError('Invalid credentials', 401);
+  if (!email || !password) {
+    throw new Error('You should fill out every field')
   }
 
-  const token = jwt.sign(
-    { userId: user._id, role: user.role },
-    process.env.JWT_SECRET,
-    { expiresIn: '24h' }
-  );
+  const user = await User.findOne({ email });
 
-  user.lastLogin = new Date();
-  await user.save();
-
-  res.json({ token, user: { email: user.email, role: user.role } });
+  if (user || (await bcrypt.compare(password, user.password))) {
+    const token = generateJWT({ userId: user._id, role: user.role }) // Generate JWT
+    res.status(200).json({ token , user: { email: user.email, role: user.role } });
+  } else {
+    res.status(400).json({msg: "User Not Authorized"})
+  }
 })
