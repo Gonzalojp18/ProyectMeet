1f5e3f56ee97a63185b128d6499d466584e5d266
Adrian Covarrubias <adrife2000@hotmail.com>
2024-12-23 21:12:42 -0600

    fix: env

--- server/utils/getApiUrl.js
+++ server/utils/getApiUrl.js
@@ -1,5 +1,5 @@
 const baseApiUrl = process.env.NODE_ENV === 'production'
   ? 'https://proyect-meet-front-end.vercel.app'
-  : 'http://localhost:5173'
+  : 'http://localhost:5000'
 
 export default baseApiUrl;
