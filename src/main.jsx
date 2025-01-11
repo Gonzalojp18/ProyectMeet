94c744e8ec0abc53aaaf2716322327f84ebf494a
AdrifeCova <adrife2000@hotmail.com>
2025-01-10 19:45:43 -0600

    feat: added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import App from './App.jsx'
 import './index.css'
 import './App.css'
@@ -7,6 +8,7 @@ import './Screen.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Analytics/>
     <App />
   </StrictMode>,
 )
